02f415d537788d5bde7a312b5e6392f2084137c8
Michael Williams <michael.williams@enspiral.com>
2023-04-28 13:12:02 +1200

    not terrible

--- README.md
+++ README.md
@@ -2,6 +2,10 @@
 
 `%k5Br27/PhR10NR9vxS89W5ljY/iMVeIr40OEW006Uvw=.sha256`
 
+- [Patchwork](https://github.com/ssbc/patchwork/)
+- [Oasis](https://github.com/fraction/oasis)
+- `wget`
+
 ```shell
 wget --mirror   --page-requisites --no-parent --html-extension --convert-links \
   -e robots=off --reject-regex ".*publish.*|.*public.*|.*mentions.*|.*search.*|.*imageSearch.*|.*settings.*|.*author.*|.*hashtag.*|.*subtopic.*|.*comment.*" \
--- index.js
+++ index.js
@@ -1,112 +1,189 @@
-import { dirname, join } from 'node:path'
+import { sep, dirname, join } from 'node:path'
 import { writeFile } from 'node:fs/promises'
 import got from 'got'
 import { mkdirp } from 'mkdirp'
 import cheerio from 'cheerio'
-import mime from 'mime-types'
+import Mime from 'mime-types'
+import { fileTypeFromBuffer } from 'file-type'
 
 export async function archive(options = {}) {
   const { outDir, host } = options
 
-  const visited = new Set()
+  const userIds = [
+    '@6ilZq3kN0F+dXFHAPjAwMm87JEb/VdB+LC9eIMW3sa0=.ed25519',
+    '@SKIc/gO6Du10rTHqujjAP1+LHzVP441U2YsQeU+Up84=.ed25519',
+    '@DUuG1ivTjZqtPYlDXHukLqsCDvjq5lkYmuZo+N4Oqc8=.ed25519',
+  ]
 
-  await downloadPageRecursively('/profile', { outDir, host, visited })
+  const visited = new Map()
+
+  for (const userId of userIds) {
+    const authorUrl = `/author/${encodeURIComponent(userId)}`
+    await downloadDoc(authorUrl, { outDir, host, visited, userIds })
+  }
+
+  const indexPage = `
+    <html>
+      <body>
+        <ul>
+          ${userIds.map(userId => (
+            `<li>
+              <a href="/author/${encodeURIComponent(userId)}.html">
+                ${userId}
+              </a>
+            </li>`
+          )).join('\n')}
+        </ul>
+      </body>
+    </html>
+  `
+  await writeFile(join(outDir, 'index.html'), indexPage)
 }
 
-async function downloadPage(url, options) {
-  const { outDir, host, visited } = options
+const processors = {
+  html: processHtml,
+}
+
+async function downloadDoc(url, options, { depth = 0 } = {}) {
+  const { outDir, host, visited, userIds } = options
+
+  if (url == null) return null
+  url = url.split('#')[0]
 
   // skip
   if (
-    visited.has(url) ||
     !url.startsWith('/') ||
     url === '/theme.css'
-  ) return [null, '']
+  ) return url
+  if (visited.has(url)) return visited.get(url)
 
-  visited.add(url, true)
+  const origUrl = url
 
-  console.log('url', url)
+  if (url.startsWith('/profile')) {
+    url = url.replace('/profile', `/author/${encodeURIComponent(userIds[0])}`)
+  }
+
   let doc
   try {
     doc = await got(url.substring(1), { prefixUrl: host })
   } catch (err) {
     console.error(err)
-    return [null, '']
+    return '/404'
   }
 
-  const ext = mime.extension(doc.headers['content-type'])
-
-  let page
-  switch (ext) {
-    case 'html':
-      page = getHtmlPage(doc.body)
-      break
-    default:
-      page = doc.body
-      break
+  let ext
+  if (doc.headers['content-type']) {
+    ext = Mime.extension(doc.headers['content-type'])
+  } else {
+    const fileType = await fileTypeFromBuffer(doc.rawBody)
+    if (fileType != null) {
+      ext = fileType.ext
+    }
   }
 
-  const path = join(
-    outDir,
-    url.includes('.')
-      ? url
-      : url + '.' + ext
-  )
+  if (url.includes('?')) {
+    const [path, params] = url.split('?')
+    if (path.endsWith('/')) {
+      url = `${path}${params}`
+    } else {
+      url = `${path}/${params}`
+    }
+  }
+  let path = decodeURIComponent(url)
+  if (ext != null && !path.endsWith('.' + ext)) {
+    url = url + '.' + ext
+    path = path + '.' + ext
+  }
+  path = join(outDir, path)
+
+  visited.set(origUrl, url)
+
+  const processor = processors[ext]
+  if (processor != null) {
+    doc = await processor(doc.body, options, { depth })
+  } else {
+    doc = doc.rawBody
+  }
 
   await mkdirp(dirname(path))
-  await writeFile(path, page)
+  await writeFile(path, doc)
 
-  return [ext, page]
+  console.log(url)
+  return url
 }
 
 const MAX_DEPTH = 1
 
-async function downloadPageRecursively(url, options, { depth = 0 } = {}) {
-  const [ext, page] = await downloadPage(url, options)
+async function processHtml(doc, options, { depth }) {
+  const { visited } = options
 
-  if (ext === 'html') {
-    const requisites = getHtmlPageRequisites(page)
-    for (const link of requisites) {
-      await downloadPage(link, options)
-    }
-
-    /*
-    if (depth < MAX_DEPTH) {
-      const links = getHtmlPageLinks(page)
-      for (const link of links) {
-        await downloadPageRecursively(link, options, { depth: depth + 1 })
-      }
-    }
-    */
-  }
-}
-
-function getHtmlPage(doc) {
   const $ = cheerio.load(doc)
+
+  // remove things
   $('body > nav').remove()
-  $('form').remove()
+  $('span:contains("This is you")').remove()
+  $('span:contains("You are following")').remove()
+  $('a:contains("Edit profile")').remove()
+  $('form:contains("Unfollow")').remove()
+
+  // transform post footer
+  $('section.post > footer > div').each((i, el) => {
+    const children = $(el).children()
+    const likes = children.first()
+    const json = children.last()
+
+    $(el).replaceWith(
+      $(`<div>
+          <form action="javascript:void(0);">
+            ${likes.find('button').prop('outerHTML')}
+          </form>
+          ${json.prop('outerHTML')}
+        </div>`)
+    )
+  })
+
+  // get page links
+  for (const link of $('link')) {
+    const url = $(link).attr('href')
+    await downloadDoc(url, options, { depth: depth + 1 })
+  }
+
+  // get page images
+  for (const img of $('img')) {
+    const url = $(img).attr('src')
+    const newUrl = await downloadDoc(url, options, { depth: depth + 1 })
+    $(img).attr('src', newUrl)
+  }
+
+  if (isPriorityProfile($, options)) {
+    for (const link of $('a:contains("Older posts")')) {
+      const url = $(link).attr('href')
+      const newUrl = await downloadDoc(url, options, { depth })
+      $(link).attr('href', newUrl)
+    }
+  }
+
+  if (depth < MAX_DEPTH) {
+    for (const link of $('a')) {
+      const url = $(link).attr('href')
+      const newUrl = await downloadDoc(url, options, { depth: depth + 1 })
+      $(link).attr('href', newUrl)
+    }
+  } else {
+    for (const link of $('a')) {
+      let url = $(link).attr('href')
+      url = url.split('#')[0]
+      $(link).attr('href', visited.get(url) || '/404')
+    }
+  }
+
   return $.html()
 }
 
-function getHtmlPageLinks(doc) {
-  const $ = cheerio.load(doc)
-  let links = []
-  $('a').each((i, elem) => {
-    links.push($(elem).attr('href'))
-  })
-  return links
-}
-
-function getHtmlPageRequisites(doc) {
-  const $ = cheerio.load(doc)
-  let links = []
-  $('link').each((i, elem) => {
-    links.push($(elem).attr('href'))
-  })
-  $('img').map((i, elem) => {
-    links.push($(elem).attr('src'))
-  })
-  return links
+function isPriorityProfile($, options) {
+  const { userIds } = options
+  const profileId = $('pre.md-mention > .hljs-link').text()
+  return userIds.includes(profileId)
 }
 
 export default archive
